5d76c74fe951649b59cb922553eff8c875892ebf
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-27 00:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let fname=document.getElementByName("fname");
+	let lname=document.getElementByName("lname");
+	alert(fname.value+" "+lname.value)
 
 }
